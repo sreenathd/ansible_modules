17d9a40b711ac5b5651f0912b4cff6716442aa84
Sreenath D <sreenathd2002@gmail.com>
2022-07-12 10:05:15 +0530

    Create user_keys.py

---
+++ modules/user_keys.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+'''
+ Description:
+  Ansible module to 
+  1. create a dictionary of all keys of a list of users
+'''
+
+import traceback
+import os
+import yaml
+import subprocess
+from ansible.module_utils.basic import AnsibleModule
+
+USERS_LIST = {
+    "users": {"required": False, "type": "list"}
+}
+
+class GenericScalar(object):
+    ''' Generic class to handle tags prefixed with exclamation mark(!) '''
+    def __init__(self, value, tag, style=None):
+        self.value = value
+        self.tag = tag
+        self.style = style
+
+    @staticmethod
+    def to_yaml(dumper, data):
+        ''' data is a generic scalar '''
+        return dumper.represent_scalar(data.tag, data.value, style=data.style)
+
+def default_constructor(loader, tag_suffix, node):
+    ''' Define generic constructor to handle tags prefixed with exclamation mark(!) '''
+    if isinstance(node, yaml.ScalarNode):
+        return GenericScalar(node.value, tag_suffix, style=node.style)
+    else:
+        raise NotImplementedError('Node: ' + str(type(node)))
+
+def validate_users(users):
+    ''' Method to validate users data '''
+    req_fields = ['name', 'state', 'sudo', 'key']
+    for usr in users:
+        for fld in req_fields:
+            if fld not in usr.keys():
+                raise ValueError('mandatory field [%s] missing in %s' % (fld, usr))
+
+            if not str(usr[fld]).strip():
+                raise ValueError('mandatory field [%s] value cannot be empty [%s]' % (fld, usr))
+
+
+def main():
+    ''' Method to process user_keys from Ansible '''
+    module = AnsibleModule(argument_spec=USERS_LIST, supports_check_mode=True)
+    errorcode = 0
+    try:
+        users_list = module.params['users']
+        bsa_key = 'id_rsa_bsa.pub'
+        res = []
+        if not users_list:
+            errorcode = 2
+            raise ValueError("'users' list cannot be empty.")
+        else:
+            validate_users(users_list)
+
+        keys_path =  os.path.join(os.getcwd(), 'sshkeys') + '/'
+             
+        for user_dict in users_list:
+            #add SSH authorized key to a dict
+            for item in user_dict['key']:
+                with open(keys_path + bsa_key , "r") as kfo:
+                    out = {}
+                    str_keyinfo = kfo.read().replace('\n', '')
+                    out[item] = str_keyinfo
+                    res.append(out)
+                
+        with open(keys_path + bsa_key , "r") as kfo:
+            out = {}
+            str_keyinfo = kfo.read().replace('\n', '')
+            out[bsa_key] = str_keyinfo
+            res.append(out)
+            
+    except : #ValueError, vearg:
+        errmsg = str(vearg) + ' stacktrace = {' + str(traceback.format_exc()) + '}'
+        module.fail_json(msg=errmsg)
+
+    localchanged = True if errorcode == 0 else False
+    module.exit_json(change=localchanged, meta=res)
+
+
+if __name__ == "__main__":
+    main()
